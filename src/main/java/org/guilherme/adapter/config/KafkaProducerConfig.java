713444a7f700a1b3df435a6e4a88d3659afb2302
guilherme <ferreiragui.dev@gmail.com>
2023-07-12 18:55:28 -0300

    Kafka Producer Config

---
+++ src/main/java/org/guilherme/adapter/config/KafkaProducerConfig.java
@@ -0,0 +1,18 @@
+package org.guilherme.adapter.config;
+
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaProducerConfig {
+
+    public static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
